04a6e5d26ff971736980c2d7739af81e26d49f3a
Ahmad Nosha Mohy Ud Din <anmd3173@gmail.com>
2026-04-04 23:04:39 +0500

    remove the mongo db config from server

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ import mongoose from 'mongoose'
 const startServer = () => {
   const PORT = process.env.PORT||5000
 
-  mongoose
-    .connect(process.env.MONGO_URI || 'mongodb://localhost:27017/shopdb')
-    .then(() => {
-      console.log('✅ MongoDB connected')
-      app.listen(PORT, () =>
-        console.log(`🚀 Server running on http://localhost:${PORT}`)
-      )
-    })
-    .catch((err) => {
-      console.error('❌ MongoDB connection failed:', err.message)
-      process.exit(1)
-    })
+  app.listen(PORT, () =>
+    console.log(`🚀 Server running on http://localhost:${PORT}`)
+  )
+  // mongoose
+  //   .connect(process.env.MONGO_URI || 'mongodb://localhost:27017/shopdb')
+  //   .then(() => {
+  //     console.log('✅ MongoDB connected')
+  //   })
+  //   .catch((err) => {
+  //     console.error('❌ MongoDB connection failed:', err.message)
+  //     process.exit(1)
+  //   })
 }
 startServer()
